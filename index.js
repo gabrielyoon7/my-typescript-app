f9b469452776874241a419e9b1a4cd40cda8fa85
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-03-02 23:53:25 +0900

    feat: 타입 에러

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 console.log('You forget a thousand things every day. Make sure this is one of them');
+var rocker;
+rocker = 'yay';
+rocker = 'Gabriel Yoon';
+console.log(rocker);
--- index.ts
+++ index.ts
@@ -1,1 +1,6 @@
 console.log('You forget a thousand things every day. Make sure this is one of them');
+
+let rocker:string;
+rocker = 'yay'; // 반드시 문자열로만 할당 가능
+rocker = 'Gabriel Yoon';
+console.log(rocker);
