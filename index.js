86dfcf98edea12aad15685dec62aa405107863f0
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-03-02 23:59:57 +0900

    feat: 타입 에러

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 console.log('You forget a thousand things every day. Make sure this is one of them');
 var rocker;
-rocker = 'yay';
+rocker = 'yay'; // 반드시 문자열로만 할당 가능
 rocker = 'Gabriel Yoon';
 console.log(rocker);
+var rapper = 'Queen Latifah';
+rapper.length;
+rapper.push('!');
--- index.ts
+++ index.ts
@@ -4,3 +4,7 @@ let rocker:string;
 rocker = 'yay'; // 반드시 문자열로만 할당 가능
 rocker = 'Gabriel Yoon';
 console.log(rocker);
+
+const rapper = 'Queen Latifah';
+rapper.length;
+rapper.push('!');
