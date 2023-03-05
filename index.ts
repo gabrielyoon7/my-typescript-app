32f88a418e82d74a1d68206f7c49b71d4bb4dbaa
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-03-05 14:02:04 +0900

    feat: 구조적 타이핑 예제 추가

--- index.ts
+++ index.ts
@@ -60,3 +60,28 @@ function normalize(v: Vector3D) {
 }
 
 console.log(normalize({ x: 3, y: 4, z: 5 })); // 그러나 에러는 안남
+
+//
+
+function calcLengthV1(v: Vector3D) {
+  let length = 0;
+  // eslint-disable-next-line no-restricted-syntax
+  for (const axis of Object.keys(v)) {
+    // eslint-disable-next-line max-len
+    const coord = v[axis]; // Element implicitly has an 'any' type because expression of type 'string' can't be used to index type 'Vector3D'.
+    // No index signature with a parameter of type 'string' was found on type 'Vector3D'.(7053)
+    length += Math.abs(coord);
+  }
+  return length;
+}
+
+function calcLengthV2(v: Vector3D) {
+  return Math.abs(v.x) + Math.abs(v.y) + Math.abs(v.z);
+}
+
+const v = {
+  x: 1, y: 2, z: 3, name: 'hi, h i~',
+};
+
+calcLengthV1(v); // name의 값이 NaN이라서 결과가 NaN으로 뜰 수 있다.
+calcLengthV2(v);
