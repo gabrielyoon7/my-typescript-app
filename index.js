e0d314f7c9d22e1b0aa0ab89f28d107fe325568c
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-03-05 13:28:18 +0900

    feat: 구조타이핑

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ var rocker;
 rocker = 'yay'; // 반드시 문자열로만 할당 가능
 rocker = 'Gabriel Yoon';
 console.log(rocker);
-var rapper = 'Queen Latifah';
-rapper.length;
-rapper.push('!');
+// const rapper = 'Queen Latifah';
+// rapper.length;
+// rapper.push('!'); // error TS2339: Property 'push' does not exist on type '"Queen Latifah"'.
--- index.ts
+++ index.ts
@@ -5,6 +5,58 @@ rocker = 'yay'; // 반드시 문자열로만 할당 가능
 rocker = 'Gabriel Yoon';
 console.log(rocker);
 
-const rapper = 'Queen Latifah';
-rapper.length;
-rapper.push('!');
+// const rapper = 'Queen Latifah';
+// rapper.length;
+// rapper.push('!'); // error TS2339: Property 'push' does not exist on type '"Queen Latifah"'.
+
+/**
+ * https://yceffort.kr/2021/06/typescript-structual-typing 예제 실습
+ */
+
+interface Vector2D {
+  x: number
+  y: number
+}
+
+function calcLength(v: Vector2D) {
+  return Math.sqrt(v.x * v.x + v.y * v.y);
+}
+
+console.log(calcLength({ x: 1, y: 2 }));
+
+//
+
+interface Vector2DWithName extends Vector2D {
+  name: string
+}
+
+const a: Vector2DWithName = { name: 'hi', x: 5, y: 10 };
+console.log(calcLength(a)); // works fine
+
+interface Vector2DName {
+  name: string
+  x: number
+  y: number
+}
+
+const b: Vector2DName = { name: 'hello', x: 10, y: 10 };
+console.log(calcLength(b)); // works fine, too.
+
+//
+
+interface Vector3D {
+  x: number
+  y: number
+  z: number
+}
+
+function normalize(v: Vector3D) {
+  const length = calcLength(v); // z가 고려되지 않음
+  return {
+    x: v.x / length,
+    y: v.y / length,
+    z: v.z / length, // z의 값이 이상하게 나옴
+  };
+}
+
+console.log(normalize({ x: 3, y: 4, z: 5 })); // 그러나 에러는 안남
