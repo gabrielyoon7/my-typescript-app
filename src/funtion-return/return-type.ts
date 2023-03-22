31e46ef7107d1ff4a93ea8d416ffa01ea29d300c
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-03-22 13:43:09 +0900

    feat: 반환 타입

---
+++ src/funtion-return/return-type.ts
@@ -0,0 +1,12 @@
+function singSongs(songs: string[]) {
+  for (const song of songs) {
+    console.log(song);
+  }
+  return songs.length;
+}
+
+function getSongAt(songs: string[], index: number) {
+  {
+    return index < songs.length ? songs[index] : undefined;
+  }
+}
