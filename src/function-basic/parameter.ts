c5c94f724451b85f7d8f051ec368fb529591d56d
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-03-30 23:00:16 +0900

    feat: 매개 변수 검사 (undefined와 함께)

--- src/function-basic/parameter.ts
+++ src/function-basic/parameter.ts
@@ -3,8 +3,24 @@ function sing(song: string) {
 }
 sing('Palms Trax');
 
+console.log('---');
+
 function singTwo(first: string, second: string) {
   console.log(`${first} / ${second}`);
 }
 
 singTwo('Palms', 'Trax');
+
+console.log('---');
+
+function announceSong(song: string, singer?: string) {
+  console.log(`Song: ${song}`);
+  if (singer) {
+    console.log(`Singer: ${singer}`);
+  }
+}
+announceSong('Cooking with Palms Trax', 'Palms Trax');
+console.log('---');
+announceSong('Cooking with Palms Trax', undefined);
+console.log('---');
+announceSong('Cooking with Palms Trax');
