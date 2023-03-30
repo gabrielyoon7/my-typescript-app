2567dd3bbf9a165b061a32e3197393ab37deb8bb
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-03-30 22:56:31 +0900

    feat: 다중 파라미터

--- src/function-basic/parameter.ts
+++ src/function-basic/parameter.ts
@@ -2,3 +2,9 @@ function sing(song: string) {
   console.log(`Singing : ${song}`);
 }
 sing('Palms Trax');
+
+function singTwo(first: string, second: string) {
+  console.log(`${first} / ${second}`);
+}
+
+singTwo('Palms', 'Trax');
