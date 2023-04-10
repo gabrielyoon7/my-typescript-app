8bbfacf0926a4e072baaa790f3272ab7cf840da8
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-04-10 18:01:05 +0900

    feat: tuple 예제 추가

---
+++ src/array/tuple.ts
@@ -0,0 +1,8 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable prefer-const */
+let yearAndWarrior: [number, string];
+yearAndWarrior = [530, 'Tomyris'];
+// yearAndWarrior = [530];
+// yearAndWarrior = [false, 'Tomyris'];
+
+let [year, warrior] = Math.random() > 0.5 ? [340, 'Archidamia'] : [1828, 'Rani of Jhamsi'];
